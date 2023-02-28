19b4551854df98f38fc2aec0fc0851ce06489416
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2023-02-28 17:10:22 +0530

    html and js file aded

---
+++ dom.js
@@ -0,0 +1,17 @@
+// console.dir(document);
+
+// console.log(document.getElementById("header-title"));
+
+let header_title = document.getElementById("header-title");
+let header = document.getElementById("main-header");
+// console.log(header_title);
+// header_title.textContent = "hello";
+// header_title.innerText = "goodbye";
+// console.log(header_title.innerText);
+// // header_title.innerHTML = "<h3>Hello</h3>";
+// header.style.borderBottom = "solid 3px #000";
+
+//get elements by class name
+
+document.getElementById("item").style.fontFamily = "bold";
+document.getElementById("item").style.color = "green";
