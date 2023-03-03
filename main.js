89543d0340c5c1693e453e29b3be020a2076711c
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2023-03-03 06:53:40 +0530

    multiple user detail can be stored in local storage

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 function print(event){
 	event.preventDefault;
-	let user_name = document.getElementById("uname").value; 
-	let email = document.getElementById("uemail").value;
-	let pas = document.getElementById("uPassword").value;
-	let dat = document.getElementById("date").value;
+	let user_name = document.getElementById("username").value; 
+	let email = document.getElementById("useremail").value;
+	let phone = document.getElementById("phone").value;
 	
 	let myobj = {
 		name : user_name,
 		email : email,
-		password : pas,
-		date : dat
+		phoneNumber : phone,
 	}
 	let serialise = JSON.stringify(myobj);
-	localStorage.setItem("obj",serialise);
+	localStorage.setItem(`${email}`,serialise);
 
 }
