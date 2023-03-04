0f6f779daa2fafe485ca4a4a9bc4fd41eeb9cc25
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2023-03-04 15:35:17 +0530

    added delete button important

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
 function print(event){
-	event.preventDefault;
+	event.preventDefault();
 	let user_name = document.getElementById("username").value; 
 	let email = document.getElementById("useremail").value;
 	let phone = document.getElementById("phone").value;
 	
-	let myobj = {
+	var myobj = {
 		name : user_name,
 		email : email,
 		phoneNumber : phone,
 	}
 	let serialise = JSON.stringify(myobj);
-	localStorage.setItem(`${email}`,serialise);
+	localStorage.setItem(`${myobj.email}`,serialise);
+   	
+
+   	
+   	let del = document.createElement("input");
+   	del.setAttribute("type","button");
+   	del.setAttribute("value","delete");
+
+    let li = document.createElement("li");
+    li.innerHTML = `${myobj.name}-${myobj.email}-${myobj.phoneNumber}`;
+    del.onclick =()=>{
+	 
+	 	li.parentNode.removeChild(li);
+	 	localStorage.removeItem(`${myobj.email}`);
+	 }
+    li.appendChild(del);
+    document.getElementById("listOfItems").appendChild(li);
+
+
+
+
+   
 
 }
