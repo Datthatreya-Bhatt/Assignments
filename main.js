a5656f2a0f94cdcca5052a307714643830af3a99
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2023-03-04 15:49:29 +0530

    added edit button important

--- main.js
+++ main.js
@@ -13,19 +13,37 @@ function print(event){
 	localStorage.setItem(`${myobj.email}`,serialise);
    	
 
-   	
+   	//creating delete button
    	let del = document.createElement("input");
    	del.setAttribute("type","button");
-   	del.setAttribute("value","delete");
+   	del.setAttribute("value","Delete");
 
+   	//creating li element
     let li = document.createElement("li");
     li.innerHTML = `${myobj.name}-${myobj.email}-${myobj.phoneNumber}`;
+    
+    //creating edit button
+    let edit = document.createElement("input");
+    edit.setAttribute("type","button");
+    edit.setAttribute("value","Edit");
+
+    //delete function
     del.onclick =()=>{
 	 
 	 	li.parentNode.removeChild(li);
 	 	localStorage.removeItem(`${myobj.email}`);
 	 }
+
+	 //edit function
+	 edit.onclick = () =>{
+	 	document.getElementById("username").value = myobj.name;
+	 	document.getElementById("useremail").value = myobj.email;
+	 	document.getElementById("phone").value = myobj.phoneNumber;
+	 	li.parentNode.removeChild(li);
+	 	localStorage.removeItem(`${myobj.email}`);
+	 }
     li.appendChild(del);
+    li.appendChild(edit);
     document.getElementById("listOfItems").appendChild(li);
 
 
