9093ed752c2f3f59647744160a02a79ecab94aef
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2023-03-31 21:08:49 +0530

    posting userdata to crudcrud

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
+
 function print(event){
 	event.preventDefault();
 	let user_name = document.getElementById("username").value; 
 	let email = document.getElementById("useremail").value;
 	let phone = document.getElementById("phone").value;
 	
-	var myobj = {
-		name : user_name,
-		email : email,
-		phoneNumber : phone,
-	}
-	let serialise = JSON.stringify(myobj);
-	localStorage.setItem(`${myobj.email}`,serialise);
+	// var myobj = {
+	// 	name : user_name,
+	// 	email : email,
+	// 	phoneNumber : phone,
+	// }
+	//let serialise = JSON.stringify(myobj);
+
+	//Posting data to crudcrud
+	axios.post("https://crudcrud.com/api/511bd323a23f481db5e5a2feeed209e8/test",{
+		name : `${user_name}`,
+		email : `${email}`,
+		phoneNumber : `${phone}`,
+	})
+	.then( (res)=>{console.log(res)})
+	.catch( (rej)=>{console.log(rej)});
    	
 
    	//creating delete button
@@ -20,7 +29,7 @@ function print(event){
 
    	//creating li element
     let li = document.createElement("li");
-    li.innerHTML = `${myobj.name}-${myobj.email}-${myobj.phoneNumber}`;
+    li.innerHTML = `${user_name}-${email}-${phone}`;
     
     //creating edit button
     let edit = document.createElement("input");
@@ -29,13 +38,13 @@ function print(event){
 
     //delete function
     del.onclick =()=>{
-	 
-	 	li.parentNode.removeChild(li);
-	 	localStorage.removeItem(`${myobj.email}`);
+	 //deleting from server
+	 	
 	 }
 
 	 //edit function
 	 edit.onclick = () =>{
+
 	 	document.getElementById("username").value = myobj.name;
 	 	document.getElementById("useremail").value = myobj.email;
 	 	document.getElementById("phone").value = myobj.phoneNumber;
@@ -52,3 +61,23 @@ function print(event){
    
 
 }
+
+
+axios.get("https://crudcrud.com/api/511bd323a23f481db5e5a2feeed209e8/test")
+.then((res)=>{
+	console.log(res,res.data[0].name);
+	
+	let length = res.data.length;
+	for(let i =0;i<length;i++){
+		//creating li element
+		console.log(length);
+	    let li = document.createElement("li");
+	    li.innerHTML = `${res.data[i].name}-${res.data[i].email}-${res.data[i].phoneNumber}`;
+	    console.log(`${res.data[i].name}-${res.data[i].email}-${res.data[i].phoneNumber}`);
+	    
+	};
+})
+.catch( (rej)=>{
+	console.log(rej);
+});
+
