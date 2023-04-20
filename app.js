99c4c4bb5fa030cde8f224a9b61c9390701f47c2
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2023-04-20 16:07:00 +0530

    custome responses created

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log(res);
+    let url = req.url;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>blank</title></head>');
+        res.write('<body><h3>This is a blank page</h3></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if(url === '/home'){
+        res.write('<html>');
+        res.write('<head><title>blank</title></head>');
+        res.write('<body><h3> Welcome home</h3></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    
+    else if(url === '/about'){
+        res.write('<html>');
+        res.write('<head><title>blank</title></head>');
+        res.write('<body><h3>Welcome to About Us page</h3></body>');
+        res.write('</html>');
+        return res.end();
+        
+    }
+    
+    else if(url === '/node'){
+        res.write('<html>');
+        res.write('<head><title>blank</title></head>');
+        res.write('<body><h3>Welcome to my node js project</h3></body>');
+        res.write('</html>');
+        return res.end();
+        
+    }
 });
 
-server.listen(4000,()=>{
-    console.log('datta');
-});
+server.listen(4000);
