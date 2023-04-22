53df8c76b669b01ffe0419c63b1d4725d4bc88a0
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2023-04-22 17:10:02 +0530

    body parser used to get input data

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('In the middelware');
-    next();//Allows the request to continue to the next middleware.
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="product"><input type="text" name="size"><button type="submit">Add product</button></form>');
 });
 
-app.use((req,res,next)=>{
-    console.log('Another middleware');
+app.post('/product',(req,res,next)=>{
+    console.log(`Product: ${req.body.product}`);
+    console.log(`Size: ${req.body.size}`); 
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
     res.send('<h1>Hello from express</h1>');
 });
 
