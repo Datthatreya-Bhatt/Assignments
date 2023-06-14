e580e188b6f794308156f7d13c9c57d5b6cb30e0
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2023-06-14 16:06:48 +0530

    leaderboard query optimized

--- fullstack expence tracker app/controller/premium.js
+++ fullstack expence tracker app/controller/premium.js
@@ -2,6 +2,7 @@ const path = require('path');
 const bcrypt = require('bcrypt');
 
 const {Expense,User} = require('../model/database');
+const { Sequelize } = require('sequelize');
 
 
 
@@ -13,56 +14,22 @@ exports.getPremiumPage = (req,res,next)=>{
 
 exports.getLeaderBoard = async(req,res,next)=>{
     
-   
   try{
 
-    let temp = await User.findAll();
-    let data = await Expense.findAll();
-
-    let obj = {};
-    data.forEach((expense)=>{
-      
-      if(obj[expense.userId]){
-        obj[expense.userId] += expense.amount;
-      }
-      else{
-        obj[expense.userId] = expense.amount;
-      }
-      
+    let leaderBoard = await User.findAll({
+      attributes:['id','name',  [Sequelize.fn('sum', Sequelize.col('amount')), 'total_expense'] ] ,
+      include: [
+        {
+          model: Expense,
+          attributes: []
+        }
+      ] ,
+      group: ['user.id'] ,
+      order: [['total_expense', 'DESC']],
     });
-
-    let leaderBoard = [];
-    temp.forEach((user)=>{
-      leaderBoard.push({name: user.name, total_expense: obj[user.id]});
-    });
-
-
-
-    // console.log(leaderBoard);
-    // console.log(obj);
     
-    
-    //sorting leaderboard
-    for(let i = 1;i<leaderBoard.length;i++){
-      let curr = leaderBoard[i];
-      let j = i-1;
-      while(j>=0 && leaderBoard[j].total_expense<curr.total_expense){
-        
-        leaderBoard[j+1] = leaderBoard[j];
-        j--;
-      } 
-      leaderBoard[j+1] = curr;
-    }
-
-    // console.log(leaderBoard);
 
 
-    if(data && temp ){
-        //console.log(data);
-    }
-    else{
-        console.log('no data in getLeaderBoard premium controller');
-    }
 
     res.send(leaderBoard);
   
